ccdc5ca74bb567ada92d4bc4971047c990de211b
NAJDAH123 <nusrathjahankazia@gmail.com>
2023-02-09 14:14:10 +0530

    after demo

--- admin.php
+++ admin.php
@@ -8,6 +8,7 @@ $query = $db->query("SELECT * FROM files ORDER BY id");
 <html>
 
 <head>
+    <title>ADMIN UPDATE</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
